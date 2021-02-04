d91076cc4d3e14a457aed9e0ede3b9e444a9c4e5
Neeka Sewnath <nsewnath@ufl.edu>
2021-02-04 08:46:50 -0500

    Initial Commit

---
+++ taxize_loop.R
@@ -0,0 +1,27 @@
+# Loop for finding corresponding family names for each genus using taxize package. 
+
+# Neeka Sewnath
+# nsewnath@ufl.edu
+
+
+# Input: Two column dataframe: One column labeled "Genus". Second column labeled "Family"
+# Switch "family" and "genus" to any taxonomic level 
+
+#install.packages("taxize")
+library(taxize)
+
+# Read csv file 
+taxa_list <- read.csv("genus_names.csv")
+
+# Loop through file, assign family name to corresponding genus
+for (i in 1:nrow(taxa_list)){
+  print(i)
+  x = tax_name(query = taxa_list$Genus[i], get = "family", db = "ncbi")
+  taxa_list$Family[i] = x[,3]
+  Sys.sleep(1)
+}
+
+# Write table
+write.csv(taxa_list, file = "genus_family_names.csv")
+
+
